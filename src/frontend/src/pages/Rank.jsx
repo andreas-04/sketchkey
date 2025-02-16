8579989d14dd159eab48d52ad923df0d287dd783
Joseph Baruch <josephbaruch48@gmail.com>
2025-02-16 01:38:11 -0800

    Table presenting rank

--- src/frontend/src/pages/Rank.jsx
+++ src/frontend/src/pages/Rank.jsx
@@ -1,26 +1,37 @@
 import { useState, useEffect } from 'react';
-import Cookies from 'js-cookie';
+// import Cookies from 'js-cookie';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography } from '@mui/material';
 
 function Rank() {
   const [rank, setRank] = useState(null);
   const [error, setError] = useState('');
 
+
+  function getCookie(name) {
+    const cookies = document.cookie.split("; ");
+    for (let cookie of cookies) {
+        const [key, value] = cookie.split("=");
+        if (key === name) {
+            return decodeURIComponent(value);
+        }
+    }
+  }
+
   const getRankings = async () => {
     setError("");
     console.clear();
     try {
-        const authCookie = Cookies.get('auth');
         const response = await fetch('http://localhost:8000/canvas/leaderboard/', {
             headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${authCookie}`
+              'Content-Type': 'application/json',
+              'Authorization': `Token ${getCookie("auth")}`,
             },
         });
 
         if (response.ok) {
             const responseData = await response.json();
-            setRank(responseData.data);
+            setRank(responseData);
+            console.log(responseData[0])
         } else {
             throw new Error('Failed to fetch rankings.');
         }
@@ -48,7 +59,7 @@ function Rank() {
                   <TableHead>
                     <TableRow>
                       <TableCell>Rank</TableCell>
-                      <TableCell>Name</TableCell>
+                      <TableCell>User</TableCell>
                       <TableCell>Score</TableCell>
                     </TableRow>
                   </TableHead>
@@ -56,8 +67,8 @@ function Rank() {
                     {rank.map((item, index) => (
                       <TableRow key={index}>
                         <TableCell>{index + 1}</TableCell>
-                        <TableCell>{item.name}</TableCell>
-                        <TableCell>{item.score}</TableCell>
+                        <TableCell>{item.user__username}</TableCell>
+                        <TableCell>{item.elo_rating}</TableCell>
                       </TableRow>
                     ))}
                   </TableBody>
